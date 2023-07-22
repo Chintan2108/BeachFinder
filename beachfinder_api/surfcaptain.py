9223ddccd6a81a76cc8447292beececb1c49754b
madalidel <115836952+madalidel@users.noreply.github.com>
2023-07-22 12:54:14 -0700

    Update surfcaptain.py

--- beachfinder_api/surfcaptain.py
+++ beachfinder_api/surfcaptain.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+#beach class
 class Beach:
+    #default constructor
     def __init__(beach):
         beach.bio="bio"
         beach.weather="weather"
@@ -9,6 +11,7 @@ class Beach:
         beach.buoy="buoy"
         beach.temp="temp"
 
+    #constructor
     def fill(beach,bio,weather,tide,buoy,temp):
         beach.bio=bio
         beach.weather=weather
@@ -16,6 +19,7 @@ class Beach:
         beach.buoy=buoy
         beach.temp=temp
     
+#get data from webpage and add to beach element
 def getBeachData(url, name):
         page=requests.get(url)
         soup=BeautifulSoup(page.content, "html.parser")
@@ -23,35 +27,10 @@ def getBeachData(url, name):
         elements=data.find_all("div", class_="current-data-desc")
         name=Beach.fill(name, "filler", elements[0].text.strip(), elements[1].text.strip(), elements[2].text.strip(), elements[3].text.strip().split("\n")[0])
         
-
+#testing
 santaBarbara = Beach()
 getBeachData("https://surfcaptain.com/forecast/santa-barbara-california", santaBarbara)
 print(santaBarbara.weather)
 print(santaBarbara.tide)
 print(santaBarbara.buoy)
 print(santaBarbara.temp)
-#Santa Barbara
-""" sb_url ="https://surfcaptain.com/forecast/santa-barbara-california"
-sb_page = requests.get(sb_url)
-
-sb_soup = BeautifulSoup(sb_page.content, "html.parser")
-
-sb_data=sb_soup.find(id="fcst-current-data")
-    #print (sb_data.prettify)
-
-sb_elements =  sb_data.find_all("div", class_= "current-data-desc")
-
-sb = Beach("filler", sb_elements[0].text.strip(), sb_elements[1].text.strip(), sb_elements[2].text.strip(), sb_elements[3].text.strip())
-print (sb.weather)
-print (sb.tide)
-print (sb.buoy)
-print (sb.temp.split("\n")[0]) """
-#Laguna 
-
-#Venice
-
-#La Jolla
-
-#Huntington
-
-#Malibu
